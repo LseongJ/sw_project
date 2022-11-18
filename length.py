8bac1085717b51d7ef707e6f1bc316bca7d677aa
LseongJ <dltjdwo8697@naver.com>
2022-11-18 12:44:53 +0900

    length클래스 뼈대코드. 각자 브랜치를 만들어서 브랜치에서 작업 후 병합 예정

---
+++ length.py
@@ -0,0 +1,8 @@
+class length:           # length클래스를 만들어서 속도, 넓이에 써먹을 예정
+    def:                                 # 함수이름은 "A_To_B"으로 통일 해주셈(A, B는 각각 단위를 나타냄)
+                                         # ex) mm를 cm로 -> mm_To_cm
+    def:
+
+    def:
+    
+    # ...
