2fcbd2756af21dbdff44adbe34581f0d230057c8
LseongJ <115673103+LseongJ@users.noreply.github.com>
2022-11-20 18:45:48 +0900

    Update and rename length_branch.py to length_뼈대.py

--- length_뼈대.py
+++ length_뼈대.py
@@ -41,9 +41,10 @@ if calculator_type == "length":
                 return num * 0.032808
             elif output_type == "inch":
                 return num * 0.393701
-        
+    # 이런식으로 input_type을 기준으로 작성하면됨
+    # 바로 밑에 출력은 맨 밑에서 할거니 출력코드 위에다가 적으면 
     print(num, input_type, "was converted to" ,length(num), output_type, end = '')            
-    #11
+   
 
 
         
